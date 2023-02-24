c29a262300a54d723fa80c5e29d4e1a9c9021552
Allauddin <allauddinraza0@gmail.com>
2023-02-24 08:11:48 +0500

    gameplayDay1

---
+++ Off Road simulation/Assets/scripts/gamemanager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour
+{
+    public GameObject _environment;
+    public void Awake()
+    {
+        _environment.SetActive(true);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
