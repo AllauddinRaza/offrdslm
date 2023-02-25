66b62f89d322985f53c0603acbc98b6564dc0f35
Allauddin <allauddinraza0@gmail.com>
2023-02-25 16:07:15 +0500

    levelselection

---
+++ Off Road simulation/Assets/scripts/LevelSelection.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class LevelSelection : MonoBehaviour
+{
+    public GameObject[] _lockLevel;
+    public GameObject[] _level;
+    //public GameObject[] _selectedLevel;
+    // Start is called before the first frame update
+    void Start()
+    {if(PlayerPrefs.GetInt("CompletedLevels") == null)
+        {
+            PlayerPrefs.SetInt("SelectedLevels", 0);
+          
+        }
+        else
+        {
+            PlayerPrefs.SetInt("SelectedLevels", PlayerPrefs.GetInt("CompletedLevels"));
+        }
+        int index = PlayerPrefs.GetInt("CompletedLevels")+1 ;
+        for (int i = index; i < 15; i++)
+        {
+            _level[i].transform.GetComponent<Button>().interactable = false;
+            _lockLevel[i].SetActive(true);
+            DOTween.Play(_level[PlayerPrefs.GetInt("SelectedLevels")]);
+        }
+    }
+
+    // Update is called once per frame
+   
+}
